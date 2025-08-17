8d1e2e5ac792f2c7488db6bdb983bcb82521621b
boyu <2584350577@qq.com>
2025-08-17 13:42:23 +0800

    first commit

---
+++ src/main/java/com/boyu/snbe/SnBeApplication.java
@@ -0,0 +1,13 @@
+package com.boyu.snbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boyu/snbe/SnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boyu.snbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
